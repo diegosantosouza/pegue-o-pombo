3112d31d37b5cfb70014c8982c5272952c0dcbd2
diegosantosouza <diegosantos.s@hotmail.com>
2023-12-16 02:16:02 -0300

    fix: ses permission

--- aws/sqs/sqs.go
+++ aws/sqs/sqs.go
@@ -3,6 +3,7 @@ package sqs
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -42,11 +43,19 @@ func DeleteSQSMessage(client *sqs.Client, arn string, receiptHandle string) erro
 // getQueueURL obtém a URL da fila SQS usando o ARN da fila.
 func getQueueURL(client *sqs.Client, arn string) (string, error) {
 	result, err := client.GetQueueUrl(context.TODO(), &sqs.GetQueueUrlInput{
-		QueueName: aws.String(arn), // Passe o ARN diretamente como QueueName
+		QueueName: aws.String(extractQueueNameFromARN(arn)),
 	})
 	if err != nil {
 		log.Printf("Error getting queue URL: %v", err)
 		return "", err
 	}
 	return *result.QueueUrl, nil
+}
+
+// extractQueueNameFromARN extrai o nome da fila do ARN da fila SQS.
+func extractQueueNameFromARN(arn string) string {
+	// Adapte a lógica de extração de nome da fila conforme necessário.
+	// Um exemplo simples: assume que o nome da fila é a parte final do ARN após o último ':'.
+	parts := strings.Split(arn, ":")
+	return parts[len(parts)-1]
 }
