995a88af867b0bbfb6804fdefb76a8509b36e5e1
diegosantosouza <diegosantos.s@hotmail.com>
2023-12-16 02:25:10 -0300

    fix: ses permission

--- aws/sqs/sqs.go
+++ aws/sqs/sqs.go
@@ -2,8 +2,9 @@ package sqs
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"strings"
+	"regexp"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -20,17 +21,12 @@ func NewSQSSession() (*sqs.Client, error) {
 }
 
 func DeleteSQSMessage(client *sqs.Client, arn string, receiptHandle string) error {
-	queueURL, err := getQueueURL(client, arn)
-	if err != nil {
-		return err
-	}
-
 	input := &sqs.DeleteMessageInput{
-		QueueUrl:      aws.String(queueURL),
+		QueueUrl:      aws.String(arnToURL(arn)),
 		ReceiptHandle: aws.String(receiptHandle),
 	}
 
-	_, err = client.DeleteMessage(context.Background(), input)
+	_, err := client.DeleteMessage(context.Background(), input)
 	if err != nil {
 		log.Printf("Error deleting message from queue: %v", err)
 		return err
@@ -40,22 +36,21 @@ func DeleteSQSMessage(client *sqs.Client, arn string, receiptHandle string) erro
 	return nil
 }
 
-// getQueueURL obtém a URL da fila SQS usando o ARN da fila.
-func getQueueURL(client *sqs.Client, arn string) (string, error) {
-	result, err := client.GetQueueUrl(context.TODO(), &sqs.GetQueueUrlInput{
-		QueueName: aws.String(extractQueueNameFromARN(arn)),
-	})
-	if err != nil {
-		log.Printf("Error getting queue URL: %v", err)
-		return "", err
-	}
-	return *result.QueueUrl, nil
-}
+func arnToURL(arn string) string {
+	// Padrão para extrair informações do ARN
+	arnPattern := regexp.MustCompile(`arn:aws:sqs:([^:]+):(\d+):(.+)`)
+	matches := arnPattern.FindStringSubmatch(arn)
 
-// extractQueueNameFromARN extrai o nome da fila do ARN da fila SQS.
-func extractQueueNameFromARN(arn string) string {
-	// Adapte a lógica de extração de nome da fila conforme necessário.
-	// Um exemplo simples: assume que o nome da fila é a parte final do ARN após o último ':'.
-	parts := strings.Split(arn, ":")
-	return parts[len(parts)-1]
+	if len(matches) != 4 {
+		return "" // Não foi possível extrair informações necessárias
+	}
+
+	region := matches[1]
+	accountID := matches[2]
+	queueName := matches[3]
+
+	// Constrói a URL da fila SQS com base nas informações extraídas
+	queueURL := fmt.Sprintf("https://sqs.%s.amazonaws.com/%s/%s", region, accountID, queueName)
+
+	return queueURL
 }
